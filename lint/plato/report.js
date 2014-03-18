8a3bb213e865e217ca5b6a3a3cbf475016f16625
uupaa <uupaa.js@gmail.com>
2014-03-18 23:12:37 +0900

    update settings.

--- lint/plato/report.js
+++ lint/plato/report.js
@@ -1,1 +1,63 @@
-__report = {"summary":{"total":{"sloc":156,"maintainability":55.19969092070768},"average":{"sloc":156,"maintainability":"55.20"}},"reports":[{"info":{"file":"lib/Clone.js","fileShort":"lib/Clone.js","fileSafe":"lib_Clone_js","link":"files/lib_Clone_js/index.html"},"jshint":{"messages":2},"complexity":{"aggregate":{"line":4,"complexity":{"sloc":{"physical":156,"logical":94},"cyclomatic":38,"halstead":{"operators":{"distinct":29,"total":207,"identifiers":["__stripped__"]},"operands":{"distinct":83,"total":258,"identifiers":["__stripped__"]},"length":465,"vocabulary":112,"difficulty":45.0722891566265,"volume":3165.4200387567857,"effort":142672.72728902573,"bugs":1.0551400129189286,"time":7926.2626271680965},"params":12}},"module":"lib/Clone.js","maintainability":55.19969092070768}}]}
+__report = {
+  "reports": [
+    {
+      "info": {
+        "file": "lib/Clone.js",
+        "fileShort": "lib/Clone.js",
+        "fileSafe": "lib_Clone_js",
+        "link": "files/lib_Clone_js/index.html"
+      },
+      "jshint": {
+        "messages": 1
+      },
+      "complexity": {
+        "aggregate": {
+          "line": 4,
+          "complexity": {
+            "sloc": {
+              "physical": 156,
+              "logical": 94
+            },
+            "cyclomatic": 38,
+            "halstead": {
+              "operators": {
+                "distinct": 29,
+                "total": 207,
+                "identifiers": [
+                  "__stripped__"
+                ]
+              },
+              "operands": {
+                "distinct": 83,
+                "total": 258,
+                "identifiers": [
+                  "__stripped__"
+                ]
+              },
+              "length": 465,
+              "vocabulary": 112,
+              "difficulty": 45.0722891566265,
+              "volume": 3165.4200387567857,
+              "effort": 142672.72728902573,
+              "bugs": 1.0551400129189286,
+              "time": 7926.2626271680965
+            },
+            "params": 12
+          }
+        },
+        "module": "lib/Clone.js",
+        "maintainability": 55.19969092070768
+      }
+    }
+  ],
+  "summary": {
+    "total": {
+      "sloc": 156,
+      "maintainability": 55.19969092070768
+    },
+    "average": {
+      "sloc": 156,
+      "maintainability": "55.20"
+    }
+  }
+}
